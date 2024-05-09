25605f28fd9279b310a04b372f85de85e80ca259
nakanishi4-yuya <nakanishi4-y@gr.m-fielders.com>
2024-05-09 16:35:59 +0900

    aa

--- gazouninsiki.py
+++ gazouninsiki.py
@@ -1,5 +1,8 @@
 
 from PIL import Image
+#pipが使えない場合システム環境変数から直接pip.exeのファイルを追加するC:\Users\Administrator\AppData\Local\Programs\Python\Python312\Scripts
 
 #　画像の読み込み 
 img = Image.open('Eチーム制作物フローチャート.drawio.png')
+
+img.show()

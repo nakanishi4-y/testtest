8933621e534247c6cc00c9ac5dd8e365ae1a31d0
nakanishi4-yuya <nakanishi4-y@gr.m-fielders.com>
2024-05-10 15:24:53 +0900

    huhuhuhuhuhuhuhuhu

---
+++ aaa.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    FILE *fp;
+    char *str = "ABC";
+    int a[3]={14,15,16};
+    fp
+}
--- gazouninsiki.py
+++ gazouninsiki.py
@@ -1,17 +1,11 @@
-#pip install Pillow
-#pip install pyocr
-#↑これをターミナルで実行
-
-
-
-from PIL import Image
+from PIL import Image, ImageTk
 import tkinter
 import tkinter.filedialog
 import os
 import sys
 import pyocr
 
-tesseract_path = "C:\Program Files\Tesseract-OCR"
+tesseract_path = r"C:\Program Files\Tesseract-OCR"  # バックスラッシュをエスケープする
 if tesseract_path not in os.environ["PATH"].split(os.pathsep):
     os.environ["PATH"] += os.pathsep + tesseract_path
 
@@ -25,30 +19,51 @@ else:
 ### 定数
 WIDTH  = 640        # 幅
 HEIGHT = 400        # 高さ
- 
+TEXT_WIDTH = 320    # テキスト表示領域の幅
+
 ### 関数
 def func():
     ### ファイルダイアログ
     name = tkinter.filedialog.askopenfilename(title="ファイル選択", initialdir="C:/", filetypes=[("Image File","*.png")])
  
     ### 画像ロード
+    global image_tk  # グローバル変数として画像を保持
     image = Image.open(name)
  
-    ### キャンバスに表示
-    canvas.create_image(WIDTH/2, HEIGHT/2, image=tkinter.PhotoImage(image))
+    ### 文字抽出
+    lang = lang_var.get()  # 選択された言語を取得
     builder = pyocr.builders.TextBuilder(tesseract_layout=6)
-    result = tool.image_to_string(image, lang="jpn", builder=builder)
-    print(result)
+    result = tool.image_to_string(image, lang=lang, builder=builder)
+    
+    ### キャンバスに画像を表示
+    image.thumbnail((TEXT_WIDTH, HEIGHT), Image.BICUBIC)  # BICUBICを使用
+    image_tk = ImageTk.PhotoImage(image)
+    canvas.delete("all")  # キャンバス内のすべての要素を削除
+    canvas.create_image(TEXT_WIDTH/2, HEIGHT/2, image=image_tk)
+    
+    ### キャンバスに文字を表示
+    canvas.create_text(TEXT_WIDTH + (WIDTH - TEXT_WIDTH)/2, HEIGHT/2, text=result, font=("Helvetica", 12), anchor="w")
 
 ### メイン画面作成
 main = tkinter.Tk()
 
 ### 画面サイズ設定
-main.geometry("640x440")
+main.geometry("960x440")
 
-### ボタン作成・配置
+### ラジオボタン用の変数
+lang_var = tkinter.StringVar(value="jpn")  # 初期値を日本語に設定
+
+### ファイル選択ボタン作成・配置
 button = tkinter.Button(main, text="ファイル選択", command=func)
-button.pack()
+button.pack(side="left")
+
+### ラジオボタン作成・配置（日本語）
+jpn_radio = tkinter.Radiobutton(main, text="日本語", variable=lang_var, value="jpn")
+jpn_radio.pack(side="left")
+
+### ラジオボタン作成・配置（英語）
+eng_radio = tkinter.Radiobutton(main, text="English", variable=lang_var, value="eng")
+eng_radio.pack(side="left")
 
 ### キャンバス作成・配置
 canvas = tkinter.Canvas(main, width=WIDTH, height=HEIGHT)
